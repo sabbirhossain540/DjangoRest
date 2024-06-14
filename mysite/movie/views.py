a642f5f22f5151f358a1a80166bc92d0ed315748
SABBIR HOSSAIN <sabbirhossain@SABBIRs-Laptop.local>
2024-06-14 17:15:01 +0900

    dd

--- mysite/movie/views.py
+++ mysite/movie/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import MovieData
 # Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.all()
+    serializer_class = MovieSerializer
+
+
+class ActionViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.filter(typ='action')
+    serializer_class = MovieSerializer
+
+class ComicViewSet(viewsets.ModelViewSet):
+    queryset = MovieData.objects.filter(typ='comic')
+    serializer_class = MovieSerializer
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'movie.apps.MovieConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+#For Image Upload
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
